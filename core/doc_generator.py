765145928b0aa2ade3892bf13a534bfe8c78e2a9
blondinkaizakon <9547apmo@gmail.com>
2025-10-06 00:04:48 +0300

    Update doc_generator.py

--- core/doc_generator.py
+++ core/doc_generator.py
@@ -1,10 +1,12 @@
+
+# core/doc_generator.py
+from core.config import TPL, GEN
 from docxtpl import DocxTemplate
-from core.config import TPL, GEN  # Импортируются правильно
 from pathlib import Path
 
 def build(data: dict, out_name: str = "contract.docx") -> Path:
-    tpl_path = Path(TPL) / "lease_ip_to_ip.docx"  # Используем Path для корректов пути
-    out_path = Path(GEN) / out_name  # Используем Path для корректов пути
+    tpl_path = Path(TPL) / "lease_ip_to_ip.docx"
+    out_path = Path(GEN) / out_name
     tpl = DocxTemplate(tpl_path)
     tpl.render(data)
     tpl.save(out_path)
