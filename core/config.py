79e05919c7c15e243b9a1580bfcd2a8770cf9578
blondinkaizakon <9547apmo@gmail.com>
2025-10-06 00:00:13 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,8 +1,7 @@
 import fitz
-
-def extract_text(pdf_path: str) -> str:
-    with fitz.open(pdf_path) as doc:
-        return "\n".join(page.get_text() for page in doc)
 TPL = "templates"  # путь к папки с шаблонами документов
 GEN = "generated"  # путь для сгенерированных документов
 TOKEN = "8440749347:AAFeXggvdBjedsTHI9cOHrHvG6vUrBnka4Y"
+def extract_text(pdf_path: str) -> str:
+    with fitz.open(pdf_path) as doc:
+        return "\n".join(page.get_text() for page in doc)
